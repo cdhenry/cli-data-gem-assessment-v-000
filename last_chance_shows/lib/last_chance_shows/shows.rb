7d6d31190f099bec0f3313f5e16792878d0bbd09
Chris Henry <cdhenry@gmail.com>
2018-02-27 00:54:05 +0000

    .closings now outputs shows with associated index

--- last_chance_shows/lib/last_chance_shows/shows.rb
+++ last_chance_shows/lib/last_chance_shows/shows.rb
@@ -3,15 +3,11 @@ class LastChanceShows::Shows
   attr_accessor :title, :venue, :closing, :url
 
   def self.closings
-    #self.scrape_closings.each_with_index(1) |show, i|
-    # puts <<-DOC.gsub /^\s*/, ""
-    #   "Closing Sunday, February 25, 2018"
-    #   "1. LATIN HISTORY FOR MORONS - Broadway: Studio 54"
-    #   "2. CARDINAL - Off-Broadway: Second Stage Theater/Tony Kiser Theatre"
-    #   "3. MILES FOR MARY - Off-Broadway: Playwrights Horizons/Peter Jay Sharp Theater"
-    #   "Closing Friday, March 2, 2018"
-    #   "4. FIRE AND AIR - Off-Broadway: Classic Stage Company"
-    # DOC
+    #outputs closings with an associated number
+    self.scrape_closings.each.with_index(1) do |show, i|
+       puts "#{i}. #{show.title}"
+       puts "     #{show.venue} // #{show.closing}"
+    end
   end
 
   def self.scrape_closings
@@ -58,68 +54,4 @@ class LastChanceShows::Shows
 
     shows
   end
-  #
-  # def self.get_venue(venue_index)
-  #   venue_p = element.parent.css("p")[venue_index].text
-  #   venue_p = doc.css(".bsp-article-content p")[venue_index].text
-  #   first = venue_p.index(": ") + 2
-  #   last = venue_p.index("\n") - 1
-  #   venue_p[first..last]
-  # end
-  #
-  # def self.scrape_playbill#(show_number)
-  #   show = self.new
-  #   doc = Nokogiri::HTML(open("http://www.playbill.com/article/last-chance-schedule-of-upcoming-broadway-and-off-broadway-show-closings"))
-  #
-  #   doc.css(".bsp-article-content h2").each do |element|
-  #     show.closing = element.text
-  #     show.title = element.parent.css()
-  #     element.parent.css("u")[show_index].text
-  #
-  #     # if element == "p"
-  #     #   if element == "u"
-  #     #     element
-  #         binding.pry
-  #       # end
-  #     #
-  #   end
-  # end
-
-
-
-    # show = self.new
-    # size_element = 2 * (show_number + 1)
-    # str1_markerstring = ": "
-    # str2_markerstring = "\n"
-    # doc = Nokogiri::HTML(open("http://www.playbill.com/article/last-chance-schedule-of-upcoming-broadway-and-off-broadway-show-closings"))
-    # doc.search('br').each { |br| br.replace("\n") }
-    #
-    # show.title = doc.css("u")[show_number].text
-    #
-    # if doc.css(".bsp-article-content p")[size_element].text.include?"Off-Broadway:"
-    #   show.size = "Off-Broadway"
-    # else
-    #   show.size = "Broadway"
-    # end
-    #
-    # venue_p = doc.css(".bsp-article-content p")[size_element].text
-    # first = venue_p.index(": ") + 2
-    # last = venue_p.index("\n") - 1
-    # show.venue = venue_p[first..last]
-    # #change show closing time to scrape previous h2
-    # nodes = doc.css(".bsp-article-content p")
-    # nodes.wrap("<div class='container'></div>")
-    # show.closing = doc.css("u")[show_number].text
-    #
-    # s_address = "http://www.playbill.com/searchpage/search?q="
-    # parse_title = show.title.split(" ")
-    # parse_title.each do |word|
-    #   s_address += word + "+"
-    # end
-    # s_address.chomp"+"
-    # s_address = s_address + "&qasset="
-    #
-    # doc2 = Nokogiri::HTML(open(s_address))
-    # show.url = "http://www.playbill.com" + doc2.css(".bsp-list-promo-title a")[0]["href"]
-  #end
 end
